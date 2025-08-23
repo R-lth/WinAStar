4ce36633d766cd4ec7db0a4fdbcd86aad07b1898
R-lth <bluearth0502@gmail.com>
2025-08-24 00:44:43 +0900

    :construction: A* 구현 중. TODO 연산자 오버로딩 및 경로 추적

---
+++ WinAPI/WinAPI/AStar.cpp
@@ -0,0 +1,98 @@
+﻿#include "AStar.h"
+
+// TODO. 경로 추적 수정
+void AStar::setPath(Position current)
+{
+	Position pos = current;
+
+	while (pos.first != -1 || pos.second != -1)
+	{
+		path.push_back(pos);
+		pos = visited[pos];
+	}
+}
+
+vector<Position> AStar::getPath()
+{
+	return path;
+}
+
+vector<Position> AStar::findPath(Position start, Position goal, const std::vector<std::vector<int>>& grid)
+{
+	// 초기화
+	Node startNode;
+	startNode.current = start;
+	startNode.parent = { -1, -1 };
+	startNode.gCost = 0;
+	startNode.hCost = heuristic(start, goal); 
+
+	// 시작
+	pq.push(startNode);
+
+	// 탐색
+	while (!pq.empty())
+	{
+		Node node = pq.top();
+		pq.pop();
+
+		// 이미 방문
+		if (visited.find(node.current) != visited.end())
+		{
+			continue;
+		}
+
+		// 이제 방문
+		visited.insert({ node.current, node.parent });
+
+		// 목적지 도착
+		Position current = { node.current.first, node.current.second };
+		if (current == goal)
+		{
+			setPath(current);
+			return path;
+		}
+
+		// 이웃 탐색
+		for (const pair<Position, float>& dir : direction)
+		{
+			int nextY = current.second + dir.first.second;
+			int nextX = current.first + dir.first.first;
+			Position nextPos = { nextX, nextY };
+
+			if (!isInRange(nextPos, grid) || grid[nextY][nextX] == 1) 
+			{
+				continue;
+			}
+
+			if (visited.find(nextPos) != visited.end())
+			{
+				continue;
+			}
+
+			Node nextNode;
+			nextNode.current = nextPos;
+			nextNode.parent = current;
+			// TODO. 비용 갱신 및 휴리스틱
+			nextNode.gCost = node.gCost + dir.second;
+			nextNode.hCost = heuristic(nextPos, goal);
+
+			pq.push(nextNode);
+		}
+	}
+
+	// 경로 없음
+	return {};
+}
+
+bool AStar::isInRange(Position pos, const std::vector<std::vector<int>>& grid)
+{
+	return (pos.first >= 0 && pos.first < static_cast<int>(grid[0].size()) &&
+		pos.second >= 0 && pos.second < static_cast<int>(grid.size()));
+}
+
+float AStar::heuristic(Position current, Position goal)
+{
+	int diffX = current.first - goal.first;
+	int diffY = current.second - goal.second;
+	return static_cast<float>(sqrt(diffX * diffX + diffY * diffY));
+}
---
+++ WinAPI/WinAPI/AStar.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include <vector>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+// 2D 격자
+using Position = std::pair<int, int>;
+
+// TODO. 구조체, 연산자 오버로딩
+struct Node
+{
+	Position current;	// 현재 위치
+	Position parent;	// 이전 위치
+
+	float gCost;		
+	float hCost;
+};
+
+class AStar
+{
+public:
+	void setPath(Position current);
+
+	vector<Position> getPath();
+
+	vector<Position> findPath(Position start, Position goal, const std::vector<std::vector<int>>& grid);
+
+private:
+	bool isInRange(Position pos, const std::vector<std::vector<int>>& grid);
+
+	float heuristic(Position current, Position goal);
+
+private:
+	const vector<pair<Position, float>> direction =
+		{
+			// 하상우좌 이동.
+			{ {0, 1}, 1.0f}, { {0, -1}, 1.0f}, { {1, 0}, 1.0f}, { {-1, 0}, 1.0f},
+			// 대각선 이동.
+			{ {1, 1}, 1.414f}, { {1, -1}, 1.414f}, { {-1, 1}, 1.414f}, { {-1, -1}, 1.414f}
+		};
+
+	priority_queue<Node, vector<Node>, greater<Node>> pq;
+	unordered_map<Position, Position> visited; 
+	vector<Position> path;
+};
