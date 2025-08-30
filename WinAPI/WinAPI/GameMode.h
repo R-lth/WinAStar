073b337c5150c37f03b0793b8159600cee9d80cf
R-lth <bluearth0502@gmail.com>
2025-08-30 11:06:34 +0900

    GameMode 제거
    레벨 간 전환이 없으므로
    https://velog.io/@sumingq/UE-Unreal-Framework-Class%EA%B0%81-%ED%81%B4%EB%9E%98%EC%8A%A4%EB%B3%84-%EC%97%AD%ED%95%A0%EC%98%88

--- WinAPI/WinAPI/GameMode.h
+++
@@ -1,5 +0,0 @@
-﻿#pragma once
-
-// 레벨 전환
-bool gameOver = false;
-bool waiting = false;
--- WinAPI/WinAPI/GameState.h
+++ WinAPI/WinAPI/GameState.h
@@ -1,5 +1,9 @@
 ﻿#pragma once
 
+// 게임 상태
+bool gameOver = false;
+bool waiting = false;
+
 // 몬스터 정보
 AStar aStar;
 vector<deque<POINT>> pathInfo;
